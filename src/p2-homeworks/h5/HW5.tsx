ce41e1b15a9e9b21865d48c0679774b3147aa744
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-02-14 22:40:56 +0300

    refactor: minor changes

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import Header from './Header'
-import Routes from './Routes'
+import Header from './Header/Header'
+import {Routings} from './Routes'
+import {HashRouter} from "react-router-dom";
 
 function HW5() {
     return (
         <div>
-            {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
-
+            <HashRouter>
             <Header/>
-
-            <Routes/>
-
-            {/*</HashRouter>*/}
+            <Routings/>
+            </HashRouter>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HW12 from "../../h12/HW12";
+
+export const JuniorPlus = () => {
+    return (
+        <div>
+            <HW12/>
+        </div>
+    );
+};
