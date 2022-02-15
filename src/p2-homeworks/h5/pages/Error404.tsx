3d93b7827de696d73e0f2725694c919c9ddf8448
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-02-16 00:30:52 +0300

    HW5: minor styles

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import s from "./Error404.module.css"
 export function Error404() {
     return (
-        <div>
+        <div className={s.wrapper}>
             <div>404</div>
             <div>Page not found!</div>
             <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
